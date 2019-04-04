bd8deedb5723162e6c62ef42f5840601980af1e4
jfiricel <jfiricel@temenos.com>
2019-04-04 15:18:35 +0300

    cleaned the gh-pages branch

--- build.bat
+++
@@ -1,3 +0,0 @@
-SET PATH=%CD%\tools\docfx;D:\Tools\wkhtmltopdf\bin;%PATH%
-docfx "%CD%\docfx_project\docfx.json" -t "%CD%\docfx_project\template\temenos" --build
-REM docfx ".\docfx_project\docfx.json" --build --port 9090
