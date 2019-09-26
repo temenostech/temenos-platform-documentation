cc41a377e37d38efd7b7b9c00f85a68a277ea149
Simona <slungu@temenos.com>
2019-09-26 16:27:49 +0300

    updated

---
+++ docfx_project/site/home/scripts/getTemplates.js
@@ -0,0 +1,50 @@
+$(window).on("load", function() {
+  // Get list of templates.
+  var templatesList = getTemplatesList();
+  // Get the id of <templates list> element from page.
+  var $templatesList = $('#templatesList');
+	var firstTemplateAdded = new Boolean(false);
+	var styleTemplate = "";
+  $.each(templatesList, function(idx, item) {
+    // Set border top and bottom for the first template of the page only.
+		if (firstTemplateAdded == true){
+			styleTemplate = "border-bottom: 2px solid gray;";
+		} else {
+			styleTemplate = "border-bottom: 2px solid gray; border-top: 2px solid gray;";
+		}
+    if (item.publicTemplate) {
+			firstTemplateAdded = true;
+      $templatesList.append('<li id="' + item.id + '" class="collapseItem" style="' + styleTemplate + '" onclick="getTemplateDetails(this.id)"><div class=""><p style="color: #005294; font-size: 16px; margin-bottom: 0px;">' + item.name + '</p><p class="card-subtitle" style="font-size: 14px;">' + item.description + '</p></div></li>');
+    }
+  })
+
+  function getTemplatesList() {
+    var templatesList;
+    //Get templates.
+    var post1 = $.ajax({
+      url: 'https://paascontroller.temenos.cloud/platform/controller-iris.svc/templates',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      type: 'GET',
+      dataType: 'json',
+      contentType: "application/json",
+      async: false,
+
+      success: function(response) {
+        templatesList = response._embedded.item;
+      },
+      error: function(errorResponse) {
+        console.log('error: ' + errorResponse);
+      },
+      done: function(results) {
+        console.log(results);
+      },
+      fail: function(jqXHR, textStatus, errorThrown) {
+        console.log('Could not get posts, server response: ' + textStatus + ': ' + errorThrown);
+      }
+    }).responseJSON;
+    return templatesList;
+  }
+});
